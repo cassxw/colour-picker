cfb3d4b65e89504c354dd84106192086369334b4
Cassandra Wallace <cassandra.wallace@flowgear.net>
2026-01-09 17:08:59 +0200

    Update index.jsx with succeeding user stories

--- index.jsx
+++ index.jsx
@@ -1,5 +1,19 @@
 const { useState } = React;
 
 export const ColorPicker = () => {
-  return ('')
+    const [color, setColor] = useState("#ffffff");
+
+    return (
+        <div
+            id="color-picker-container"
+            style={{ backgroundColor: color }}
+        >
+            <input
+                id="color-input"
+                type="color"
+                value={color}
+                onChange={(e) => setColor(e.target.value)}
+            />
+        </div>
+    );
 };
